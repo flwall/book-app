4441f6070510bbbdb66dee3d8e6ef6163c98090c
Florian Wall <20160096@students.htl-perg.ac.at>
2019-12-22 21:42:16 +0100

    added some READMEs

--- book_api/src/main/java/com.waflo/UploadResource.java
+++ book_api/src/main/java/com.waflo/UploadResource.java
@@ -10,28 +10,62 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.*;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
+
 @Path("/upload")
 public class UploadResource {
 
+    private static final String UPLOADED_FILE_PATH = "./uploaded/";
     private final Logger logger;
 
     public UploadResource() {
         this.logger = LoggerFactory.getLogger(UploadResource.class);
+
+        new File(UPLOADED_FILE_PATH).mkdirs();
     }
 
     @POST
     @Path("/cover")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
+
     public Response uploadCover(MultipartFormDataInput input, @Context HttpHeaders headers) {
-        return Response.ok().build();
+        String fileName = "";
 
+        Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
+        List<InputPart> inputParts = uploadForm.get("coverFile");
 
+        for (InputPart inputPart : inputParts) {
+
+            try {
+
+                MultivaluedMap<String, String> header = inputPart.getHeaders();
+                fileName = getFilename(header);
+
+                //convert the uploaded file to inputstream
+                InputStream inputStream = inputPart.getBody(InputStream.class, null);
+
+                byte[] bytes = IOUtils.toByteArray(inputStream);
+
+                //constructs upload file path
+                fileName = UPLOADED_FILE_PATH + fileName;
+
+                IOUtils.write(bytes, new FileOutputStream(fileName));
+
+                System.out.println("Done");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return Response.accepted().entity("Cover uploaded successfully!").build();
     }
 
     @Consumes(MediaType.MULTIPART_FORM_DATA)
@@ -39,7 +73,7 @@ public class UploadResource {
     public Response uploadBook(MultipartFormDataInput input, @Context HttpHeaders headers) {
 
         Map<String, List<InputPart>> formDataMap = input.getFormDataMap();
-        List<InputPart> inputParts = formDataMap.get("book-file");//name of the file uploaded has to be like this
+        List<InputPart> inputParts = formDataMap.get("bookFile");//name of the file uploaded has to be like this
 
         for (var part : inputParts) {
 
